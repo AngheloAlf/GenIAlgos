9fbba053d01ec16f71f8d285b9e4243d3b2c1c67
angie <angheloalf95@gmail.com>
2020-06-10 02:07:53 -0400

    unnecesary changes

--- src/bmp/toBmp.c
+++ src/bmp/toBmp.c
@@ -16,34 +16,42 @@ void write_4_bytes(FILE *f, uint32_t val){
     fwrite(&val, 4, 1, f);
 }
 
+typedef struct{
+    size_t array_size;
+    const uint8_t *raw_array;
+    size_t bpp;
+    uint32_t width, height;
+    const uint32_t *colors;
+} RawBitIndexedImg;
 
-uint32_t offset_to_start(size_t bpp){
-    return 14 + 40 + 4*MAX_COLORS(bpp);
+
+uint32_t offset_to_start(const RawBitIndexedImg *img_data){
+    return 14 + 40 + 4*MAX_COLORS(img_data->bpp);
 }
-uint32_t file_size(size_t bpp, uint32_t width, int32_t height){
+uint32_t file_size(const RawBitIndexedImg *img_data){
     // TODO: Consider add support to 16, 24 and 32 bpp.
-    const size_t bytes_per_row = width/PIXELS_PER_BYTE(bpp) + !!(width%(size_t)(PIXELS_PER_BYTE(bpp)));
-    const uint32_t padding = (4 - (bytes_per_row % 4))%4;
+    const size_t bytes_per_row = img_data->width/PIXELS_PER_BYTE(img_data->bpp) + !!(img_data->width%PIXELS_PER_BYTE(img_data->bpp));
+    const uint32_t padding = BMP_BYTES_PADDING(bytes_per_row);
 
-    return offset_to_start(bpp) + (bytes_per_row + padding) * height;
+    return offset_to_start(img_data) + (bytes_per_row + padding) * img_data->height;
 }
 
-void write_file_header(FILE *f, size_t bpp, uint32_t width, int32_t height){
+void write_file_header(FILE *f, const RawBitIndexedImg *img_data){
     fwrite("BM", 1, 2, f); // bfType
-    write_4_bytes(f, file_size(bpp, width, height)); // bfSize
+    write_4_bytes(f, file_size(img_data)); // bfSize
     write_2_bytes(f, 0); // bfReserved1
     write_2_bytes(f, 0); // bfReserved2
-    write_4_bytes(f, offset_to_start(bpp)); // bfOffBits
+    write_4_bytes(f, offset_to_start(img_data)); // bfOffBits
 }
 
 
-void write_image_header(FILE *f, size_t bpp, uint32_t width, int32_t height){
+void write_image_header(FILE *f, const RawBitIndexedImg *img_data){
     const uint64_t header_size = 40;
     write_4_bytes(f, header_size); // biSize
-    write_4_bytes(f, width); // biWidth
-    write_4_bytes(f, height); // biHeight
+    write_4_bytes(f, img_data->width); // biWidth
+    write_4_bytes(f, img_data->height); // biHeight
     write_2_bytes(f, 1); // biPlanes
-    write_2_bytes(f, bpp); // biBitCount
+    write_2_bytes(f, img_data->bpp); // biBitCount
 
     write_4_bytes(f, 0); // biCompression
     write_4_bytes(f, 0); // biSizeImage
@@ -53,24 +61,24 @@ void write_image_header(FILE *f, size_t bpp, uint32_t width, int32_t height){
     write_4_bytes(f, 0); // biClrImportant
 }
 
-void write_color_table(FILE *f, size_t bpp, const uint32_t *colors){
+void write_color_table(FILE *f, const RawBitIndexedImg *img_data){
     /// colors array must have at least (2**bpp) elements.
     /// color = [ZERO][RED][GREEN][BLUE]
-    fwrite(colors, 4, MAX_COLORS(bpp), f);
+    fwrite(img_data->colors, 4, MAX_COLORS(img_data->bpp), f);
 }
 
-void write_pixel_data(FILE *f, size_t arr_size, const uint8_t *in_arr, size_t bpp, uint32_t width, uint32_t height){
-    const size_t pixels_per_byte = PIXELS_PER_BYTE(bpp);
-    if(width % (4*pixels_per_byte) == 0){
-        fwrite(in_arr, 1, arr_size, f);
+void write_pixel_data(FILE *f, const RawBitIndexedImg *img_data){
+    const size_t pixels_per_byte = PIXELS_PER_BYTE(img_data->bpp);
+    if(img_data->width % (BMP_BYTE_BOUNDARY*pixels_per_byte) == 0){
+        fwrite(img_data->raw_array, 1, img_data->array_size, f);
         return;
     }
 
-    const size_t bytes_per_row = width/pixels_per_byte + !!(width%pixels_per_byte);
-    const uint32_t padding = (4 - (bytes_per_row % 4))%4;
+    const size_t bytes_per_row = img_data->width/pixels_per_byte + !!(img_data->width%pixels_per_byte);
+    const uint32_t padding = BMP_BYTES_PADDING(bytes_per_row);
     const uint32_t zero = 0;
-    for(size_t i = 0; i < arr_size; i+=bytes_per_row){
-        fwrite(&in_arr[i], 1, bytes_per_row, f);
+    for(size_t i = 0; i < img_data->array_size; i+=bytes_per_row){
+        fwrite(&img_data->raw_array[i], 1, bytes_per_row, f);
         fwrite(&zero, padding, 1, f);
     }
 }
@@ -80,9 +88,19 @@ void toBmp_bitIndexed(const char *out_file, size_t arr_size, const uint8_t *in_a
     assert(bpp <= 8);
     FILE *f = fopen(out_file, "wb");
     assert(f != NULL);
-    write_file_header(f, bpp, width, height);
-    write_image_header(f, bpp, width, height);
-    write_color_table(f, bpp, colors);
-    write_pixel_data(f, arr_size, in_arr, bpp, width, height);
+
+    RawBitIndexedImg image = {
+        .array_size = arr_size,
+        .raw_array = in_arr,
+        .bpp = bpp,
+        .width = width,
+        .height = height,
+        .colors = colors
+    };
+
+    write_file_header(f, &image);
+    write_image_header(f, &image);
+    write_color_table(f, &image);
+    write_pixel_data(f, &image);
     fclose(f);
 }
--- src/bmp/toBmp.h
+++ src/bmp/toBmp.h
@@ -5,8 +5,11 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define MAX_COLORS(_bpp)        (1 << (_bpp))
-#define PIXELS_PER_BYTE(_bpp)   (8/(_bpp))
+#define MAX_COLORS(_bpp)                    (1 << (_bpp))
+#define PIXELS_PER_BYTE(_bpp)               (8/(_bpp))
+
+#define BMP_BYTE_BOUNDARY                   4
+#define BMP_BYTES_PADDING(_bytes_per_row)   ((BMP_BYTE_BOUNDARY - ((_bytes_per_row) % BMP_BYTE_BOUNDARY))%BMP_BYTE_BOUNDARY)
 
 
 void toBmp_bitIndexed(const char *out_file, size_t arr_size, const uint8_t *in_arr, size_t bpp, uint32_t width, int32_t height, const uint32_t *colors);
