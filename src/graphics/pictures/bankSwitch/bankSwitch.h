7d8b52c7cd05b71bc4471293dc58d1f5303680d0
angie <angheloalf95@gmail.com>
2020-06-12 21:31:32 -0400

    Picture's banks by pk id

---
+++ src/graphics/pictures/bankSwitch/bankSwitch.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef GRAPHICS_SPRITES_BANKSWITCH_H
+#define GRAPHICS_SPRITES_BANKSWITCH_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+
+size_t getRBankByPkId(size_t pkId, bool y_mode);
+
+
+#endif /* GRAPHICS_SPRITES_BANKSWITCH_H */
---
+++ src/graphics/pictures/bankSwitch/getRBankByPkId.c
@@ -0,0 +1,26 @@
+#include "bankSwitch.h"
+
+
+size_t getRBankByPkId(size_t pkId, bool y_mode){
+    if(y_mode && pkId == 0x15){
+        return 0x01;
+    }
+    if(pkId == 0xB6){
+        return 0x0B;
+    }
+
+    if(pkId < 0x1F){
+        return 0x09;
+    }
+    if(pkId < 0x4A){
+        return 0x0A;
+    }
+    if(pkId < 0x74){
+        return 0x0B;
+    }
+    if(pkId < 0x99){
+        return 0x0C;
+    }
+    return 0x0D;
+}
+
