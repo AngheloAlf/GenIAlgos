232bb1c4deb694f2a67d262d134ef243ba381fd0
angie <angheloalf95@gmail.com>
2020-06-27 17:31:45 -0400

    Decompression algorithm

--- src/graphics/pictures/picCompression/picCompression.h
+++ src/graphics/pictures/picCompression/picCompression.h
@@ -6,7 +6,7 @@
 #include <stdint.h>
 
 
-void decompressPicture(size_t dst_size, uint8_t *dst_buffer0, uint8_t *dst_buffer1, size_t compressed_size, const uint8_t *compressed_arr, size_t width_in_tiles, size_t height_in_tiles, size_t bytes_per_tile);
+void decompressPicture(size_t dst_size, uint8_t *dst_buffer0, uint8_t *dst_buffer1, size_t compressed_size, /*const */uint8_t *compressed_arr);
 
 
 #endif /* GRAPHICS_PICTURES_PICCOMPRESSION_H */
---
+++ src/graphics/pictures/picCompression/picCompression_test.c
@@ -0,0 +1,36 @@
+#include "picCompression.h"
+
+#include <stdlib.h>
+#include <assert.h>
+
+#include "utils/io/io.h"
+
+
+#define BUFFER_SIZE             0x188
+
+
+#define UNDERLINE(_text) "\e[4m"_text"\e[0m"
+void check_arguments(int argc, char **argv){
+    if(argc < 2){
+        printf("Usage: %s %s\n", argv[0], UNDERLINE("compressed_sprite_file"));
+        exit(-1);
+    }
+}
+
+int main(int argc, char **argv){
+    check_arguments(argc, argv);
+
+    const char *filename = argv[1];
+
+    size_t size = getFileSize(filename);
+    uint8_t *compressed = calloc(size, sizeof(uint8_t));
+    readFile(compressed, size, filename, 0);
+
+    uint8_t buffer[2*BUFFER_SIZE];
+    decompressPicture(BUFFER_SIZE, buffer, &buffer[BUFFER_SIZE], size, compressed);
+
+    writeFile(buffer, 2*BUFFER_SIZE, "buffer_dump.bin", 0);
+
+    free(compressed);
+    return 0;
+}
